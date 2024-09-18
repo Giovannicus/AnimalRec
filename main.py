c8cf9b411b3e21c452ecd87affd131276b934454
Giovannicus <giovannicus900@gmail.com>
2024-09-18 12:45:20 +0200

    Comments added

---
+++ main.py
@@ -0,0 +1,9 @@
+# type hinting
+# importare solo le funzioni utili dalle librerie e raggruppa per tali
+# importare dati con chunksize
+# monitora uso memoria
+# grafici come funzioni
+# gestisci errori con eccezioni
+# pygwal
+# visualkeras
+# prova applicativo plotly
